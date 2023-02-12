d8aafb25e261a9197900ca424ae7f8985f541dc5
Edwinngera <edwinngera@gmail.com>
2023-02-12 13:00:32 +0300

    added post serializer

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api_home, name='index'),]
+    path('', views.api_home, name='index'),
+    path('v1/', views.api_test, name='test')]
+    
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,6 +5,8 @@ from products.models import Product
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
+
 
 
 
@@ -21,6 +23,26 @@ def api_home(request, *args, **kwargs):
     if model_data:
         data=model_to_dict(model_data,fields=['id', 'title','price'])
     return Response(data)
+
+#Test Serializers
+@api_view(["GET"])
+def api_test(request, *args, **kwargs):
+    instance=Product.objects.all().order_by("?").first()
+    data={}
+    if instance:
+        data=ProductSerializer(instance).data
+    return Response(data)
+
+@api_view(["POST"])
+def api_create(request, *args, **kwargs):
+    data=request.data
+    serializer=ProductSerializer(data=data)
+    if serializer.is_valid():
+        print(serializer.data)
+        data=serializer.data
+    return Response(data)
+
+
 
   
 
